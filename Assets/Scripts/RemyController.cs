4279bfb2ccfb9c99fde2015c4c31bcf2d02764ad
Shubham <sharmashubham9800@gmail.com>
2018-07-25 19:21:29 +0530

    Initial commit

---
+++ Assets/Scripts/RemyController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+public class RemyController : MonoBehaviour {
+	public GameObject player;
+	public GameObject arrow;
+	bool reached;
+	Animator Remy;
+	NavMeshAgent agent;
+	public GameObject target;
+	public GameObject Dialoguebox;
+	public GameObject text;
+	Text txt;
+	// Use this for initialization
+	void Start () {
+	txt=text.GetComponent<Text>();
+	Dialoguebox.SetActive(false);
+	agent = GetComponent<NavMeshAgent>();
+	Remy=gameObject.GetComponent<Animator>();
+	arrow.SetActive(false);
+	Invoke("Walk",5.0f);
+	reached=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator Talk()
+    {	yield return new WaitForSeconds(4.0f);
+		Remy.SetBool("idle",false);
+		gameObject.transform.LookAt(player.transform);
+		Dialoguebox.SetActive(true);
+		txt.text="Hey!!!";
+		Remy.SetTrigger("talk");
+        yield return new WaitForSeconds(4.0f);
+		Remy.SetTrigger("talk");
+		txt.text="Yes!!"+"\n You Over There";
+		Remy.SetTrigger("talk");
+        yield return new WaitForSeconds(4.0f);
+		Remy.SetTrigger("talk");
+		txt.text="Won't You come with me";
+        yield return new WaitForSeconds(4.0f);
+		Remy.SetTrigger("talk");
+		txt.text="Come On!! \n"+"Don't be shy";
+		yield return new WaitForSeconds(4.0f);
+		Dialoguebox.SetActive(false);
+		arrow.SetActive(true);
+        
+    }
+
+	void Walk()
+	{
+		Remy.SetBool("walk",true);
+        agent.destination = target.transform.position;
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag=="target")
+		{
+		Remy.SetBool("idle",true);
+		StartCoroutine("Talk");
+		Destroy(other.gameObject);
+		}
+		
+	}
+
+	
+
+}
---
+++ Assets/Scripts/ragdollController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragdollController : MonoBehaviour {
+
+	public Animator anim;
+
+	public Rigidbody[] rbs;
+
+	public Rigidbody head, groin;
+
+	// Use this for initialization
+	void Start () {
+		rbs = GetComponentsInChildren<Rigidbody> ();
+		foreach (Rigidbody rb in rbs) {
+			rb.isKinematic = true;
+			rb.useGravity = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			anim.enabled = false;
+			foreach (Rigidbody rb in rbs) {
+				rb.isKinematic = false;
+				rb.useGravity = true;
+			}
+
+			head.AddForce (Vector3.right * 5000);
+
+		}
+	}
+}
